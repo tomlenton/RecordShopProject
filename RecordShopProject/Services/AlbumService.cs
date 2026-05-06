a93b7ef3f290ecd77b94f0f74a4eed0618766861
tomlenton <twlenton@gmail.com>
2026-05-06 13:38:09 +0100

    Feat: Added Album Service and AddAlbum method

---
+++ RecordShopProject/Services/AlbumService.cs
@@ -0,0 +1,32 @@
+﻿using RecordShopProject.DataModels;
+using RecordShopProject.Repositories;
+
+namespace RecordShopProject.Services
+{
+    public interface IAlbumService
+    {
+        IEnumerable<Album> GetAllAlbums();
+        Album? GetAlbumById(int id);
+        Album AddAlbum(Album newAlbum);
+    }
+    public class AlbumService : IAlbumService
+    {
+        private readonly IAlbumRepository _albumRepository;
+        public AlbumService(IAlbumRepository albumRepository)
+        {
+            _albumRepository = albumRepository;
+        }
+        public IEnumerable<Album> GetAllAlbums()
+        {
+            return _albumRepository.GetAllAlbums();
+        }
+        public Album? GetAlbumById(int id)
+        {
+            return _albumRepository.GetAlbumById(id);
+        }
+        public Album AddAlbum(Album newAlbum)
+        {
+            return _albumRepository.AddAlbum(newAlbum);
+        }
+    }
+}
