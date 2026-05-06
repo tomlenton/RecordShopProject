22a11c889e8aa97ce112f7eda486422e5a356c51
tomlenton <twlenton@gmail.com>
2026-05-06 13:37:12 +0100

    Feat: Added Album Repository and GetAllAlbums method

---
+++ RecordShopProject/Repositories/AlbumRepository.cs
@@ -0,0 +1,34 @@
+﻿using RecordShopProject.DataModels;
+using RecordShopProject.DBContext;
+
+namespace RecordShopProject.Repositories
+{
+    public interface IAlbumRepository
+    {
+        List<Album> GetAllAlbums();
+        Album AddAlbum(Album newAlbum);
+        Album? GetAlbumById(int id);
+    }
+    public class AlbumRepository : IAlbumRepository
+    {
+        private readonly RecordShopContext _dbContext;
+        public AlbumRepository(RecordShopContext db)
+        {
+            _dbContext = db;
+        }
+        public List<Album> GetAllAlbums()
+        {
+            return _dbContext.Albums.ToList();
+        }
+        public Album? GetAlbumById(int id)
+        {
+            return _dbContext.Albums.FirstOrDefault(a => a.AlbumId == id);
+        }
+        public Album AddAlbum(Album newAlbum)
+        {
+            _dbContext.Albums.Add(newAlbum);
+            _dbContext.SaveChanges();
+            return newAlbum;
+        }
+    }
+}
