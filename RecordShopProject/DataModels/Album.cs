d5ba8a9a4b0be83336e7d8d012f926039f5f17ca
tomlenton <twlenton@gmail.com>
2026-05-06 11:17:27 +0100

    Feat: Added Album class with properties

---
+++ RecordShopProject/DataModels/Album.cs
@@ -0,0 +1,12 @@
+﻿namespace RecordShopProject.DataModels
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public int Stock { get; set; }
+    }
+}
