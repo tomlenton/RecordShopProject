750f2f965ba1f05c81121d4ae3fa2dcf3de4f2a5
tomlenton <twlenton@gmail.com>
2026-05-06 11:19:40 +0100

    Feat: add Entity Framework Core setup, RecordShopContext, and initial migration

---
+++ RecordShopProject/DBContext/RecordShopContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordShopProject.DataModels;
+
+namespace RecordShopProject.DBContext
+{
+   public class RecordShopContext : DbContext
+   {
+        public DbSet<Album> Albums { get; set; }
+        public RecordShopContext(DbContextOptions<RecordShopContext> options) : base(options) {}
+   }
+}
---
+++ RecordShopProject/Migrations/20260506101002_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordShopProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    AlbumId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Artist = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseYear = table.Column<int>(type: "int", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.AlbumId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albums");
+        }
+    }
+}
